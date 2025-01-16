4556d21f4c309ee643faee8d0d808f50e1f97671
allan hans <allan.hans68350@gmail.com>
2025-01-16 13:32:13 +0100

    wait

--- sources/ServerIdentification.cpp
+++ sources/ServerIdentification.cpp
@@ -19,9 +19,12 @@ bool	Server::identPass(int clientFd){
 	std::string	wholePassw(passRecv);
 	std::string	cmd(passRecv);
 
-	if(wholePassw.find("\r\n") == std::string::npos){
-		wholePassw = wholePassw.substr(0, wholePassw.size() - 1) + "\r\n";
-	}
+	size_t pos = wholePassw.find("\r\n");
+	if (pos != std::string::npos)
+		wholePassw = wholePassw.substr(0, pos);
+	pos = wholePassw.find("\n");
+	if (pos != std::string::npos)
+		wholePassw = wholePassw.substr(0, pos);
 
 	if(cmd.empty() == false && cmd.size() > 5)
 		cmd = cmd.substr(0, 4);
@@ -29,8 +32,11 @@ bool	Server::identPass(int clientFd){
 		return false;
 
 	if(cmd.compare("PASS") == 0){
-		wholePassw = wholePassw.substr(5, wholePassw.size() - (wholePassw.size() - 5));
-		if(wholePassw.compare(server.getPassw() + "\r\n") == 0)
+		wholePassw = wholePassw.substr(5, wholePassw.size() - 5);
+		// std::cout << "wholePassw: " << wholePassw << "test" << std::endl;
+		// std::cout << "server.getPassw(): " << server.getPassw() << "test" << std::endl;
+		// std::cout << wholePassw.compare(server.getPassw()) << std::endl;
+		if(wholePassw.compare(server.getPassw()) == 0)
 			return true;
 	}
 	std::string error = ":server_pika 464 * :Incorrect password\r\n";
@@ -38,6 +44,42 @@ bool	Server::identPass(int clientFd){
 	return false;
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// bool	Server::identPass(int clientFd){
+
+// 	Server	&server = Server::getInstance();
+
+// 	char	passRecv[100] = {0};
+// 	ssize_t	bytesRead;
+
+// 	bytesRead = recv(clientFd, passRecv, sizeof(passRecv), 0);
+// 	if(bytesRead == -1){
+// 		std::cerr << "ERROR :password's recv doesn't work." << std::endl;
+// 		return false;
+// 	}
+
+// 	std::string	wholePassw(passRecv);
+// 	std::string	cmd(passRecv);
+
+// 	if(wholePassw.find("\r\n") == std::string::npos){
+// 		wholePassw = wholePassw.substr(0, wholePassw.size() - 1) + "\r\n";
+// 	}
+
+// 	if(cmd.empty() == false && cmd.size() > 5)
+// 		cmd = cmd.substr(0, 4);
+// 	else
+// 		return false;
+
+// 	if(cmd.compare("PASS") == 0){
+// 		wholePassw = wholePassw.substr(5, wholePassw.size() - 5);
+// 		if(wholePassw.compare(server.getPassw() + "\r\n") == 0)
+// 			return true;
+// 	}
+// 	std::string error = ":server_pika 464 * :Incorrect password\r\n";
+// 	send(clientFd, error.c_str(), error.size(), 0);
+// 	return false;
+// }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 bool	Server::askNickname(int clientFd){
 
@@ -45,7 +87,6 @@ bool	Server::askNickname(int clientFd){
 
 	char	cmdNickname[100] = {0};
 	ssize_t	bytesRead;
-	std::string	welcome;
 
 	bytesRead = recv(clientFd, cmdNickname, sizeof(cmdNickname), 0);
 	if(bytesRead == -1){
@@ -54,26 +95,25 @@ bool	Server::askNickname(int clientFd){
 	}
 
 	std::string	tmp(cmdNickname);
-	if(tmp.find("\r\n") == std::string::npos){
-		tmp = tmp.substr(0, tmp.size() - 1) + "\r\n";
-	}
 
+	size_t pos = tmp.find("\r\n");
+	if (pos != std::string::npos)
+		tmp = tmp.substr(0, pos);
+	pos = tmp.find("\n");
+	if (pos != std::string::npos)
+		tmp = tmp.substr(0, pos);
 	std::string	wholeCmd;
 	std::string	cmd = tmp;
 	std::string	nickname;
 
 	if(cmd.empty() == false && cmd.size() > 5)
 		cmd = cmd.substr(0, 4);
-	if(cmd.compare("NICK") == 0){
-		nickname = tmp.substr(5, tmp.size() - (tmp.size() - 5) - 1);
-		std::cout << nickname << std::endl;
-		std::cout << nickname.size() << std::endl;
+	if(cmd.compare("NICK") == 0) {
+		nickname = tmp.substr(5, (tmp.size() - 5));
 		server.getUser(clientFd).setNickname(nickname);
-	}
-	else{
+	} else {
 		return false;
 	}
-
 	wholeCmd = ":server_pika NICK " + nickname;
 	wholeCmd.append("\r\n");
 
@@ -81,7 +121,46 @@ bool	Server::askNickname(int clientFd){
 	return true;
 }
 
+	////////////////////////////////////////////////////////
+// bool	Server::askNickname(int clientFd){
+// 	Server	&server = Server::getInstance();
 
+// 	char	cmdNickname[100] = {0};
+// 	ssize_t	bytesRead;
+// 	std::string	welcome;
+
+// 	bytesRead = recv(clientFd, cmdNickname, sizeof(cmdNickname), 0);
+// 	if(bytesRead == -1){
+// 		std::cerr << "ERROR : nickname's recv doesn't work." << std::endl;
+// 		return false;
+// 	}
+
+// 	std::string	tmp(cmdNickname);
+// 	if(tmp.find("\r\n") == std::string::npos){
+// 		tmp = tmp.substr(0, tmp.size() - 1) + "\r\n";
+// 	}
+
+// 	std::string	wholeCmd;
+// 	std::string	cmd = tmp;
+// 	std::string	nickname;
+
+// 	if(cmd.empty() == false && cmd.size() > 5)
+// 		cmd = cmd.substr(0, 4);
+// 	if(cmd.compare("NICK") == 0){
+// 		nickname = tmp.substr(5, tmp.size() - 5);
+// 		server.getUser(clientFd).setNickname(nickname);
+// 	}
+// 	else{
+// 		return false;
+// 	}
+
+// 	wholeCmd = ":server_pika NICK " + nickname;
+// 	wholeCmd.append("\r\n");
+
+// 	send(clientFd, wholeCmd.c_str(), wholeCmd.size(), 0);
+// 	return true;
+// }
+////////////////////////////////////////////////////////////////////
 bool	Server::askUser(int clientFd){
 
 	char cmdUser[100] = {0};
@@ -155,9 +234,9 @@ bool	Server::identification(int clientFd){
 	send(clientFd, welcome.c_str(), welcome.size(), 0);
 	welcome = ":server_pika 003 " + server.getUser(clientFd).getNickname() + " :This server was created today\r\n";
 	send(clientFd, welcome.c_str(), welcome.size(), 0);
-	welcome = ":server_pika 004 " + server.getUser(clientFd).getNickname() + " server_pika 1.0 itkol\r\n";
+	welcome = ":server_pika 004 " + server.getUser(clientFd).getNickname() + " :server_pika 1.0 itkol\r\n";
 	send(clientFd, welcome.c_str(), welcome.size(), 0);
-	welcome = ":server_pika 005 " + server.getUser(clientFd).getNickname() + " :are supported by this server CHANTYPES=# PREFIX=(ov)@ CHANNELLEN=32 NICKLEN=9 TOPICLEN=307 \r\n";
+	welcome = ":server_pika 005 " + server.getUser(clientFd).getNickname() + " :are supported by this server CHANTYPES=# PREFIX=(ov)@ CHANNELLEN=32 NICKLEN=9 TOPICLEN=307\r\n";
 	send(clientFd, welcome.c_str(), welcome.size(), 0);
 
 	return true;
